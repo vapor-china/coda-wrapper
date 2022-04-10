71dc63fb3c71b63fbf2ceb2eae1b971a3a94ce20
spectatorMrZ <zhuchunan001@sina.com>
2022-04-10 17:01:49 +0800

    mark key coding container url

---
+++ Sources/Coda/KeyedCodingContainer.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  
+//
+//  Created by Aneya on 2022/4/10.
+//
+
+// https://stackoverflow.com/questions/44603248/how-to-decode-a-property-with-type-of-json-dictionary-in-swift-45-decodable-pr
+
+import Foundation
